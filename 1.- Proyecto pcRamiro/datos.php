66d315f0afd5968119da8c0a6e55133dacd63d0f
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2026-09-16 14:46:15 +0200

    datos

---
+++ 1.- Proyecto pcRamiro/datos.php
@@ -0,0 +1,16 @@
+<?php
+
+    const IVA_GENERAL = 0.21; // Siempre nombres en mayus con _ para const
+    const UNIDADES_DESCUENTO = 5;
+    const DESCUENTO_CANTIDAD = 0.10;
+
+    $productos = [
+        ["id" => 1,
+        "nombre" => "Teclado",
+        "categoria" => "perifericos",
+        "precio" => 7990,
+        "stock" => 7]
+    ]; // lenguaje no tipado, como JS. Esto es una colección de objetos con clave valor, se llama en este caso arraw asociativo.
+
+
+?>
